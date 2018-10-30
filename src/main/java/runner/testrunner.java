514f541072fca60fee887249084919ce2264d986
Jayshri Patel <jayshri.patel@mirumagency.com>
2018-10-30 11:40:23 +0530

    Changed Crome driver Path in Main.java file to use URLsetup() method commonly

--- src/main/java/runner/testrunner.java
+++ src/main/java/runner/testrunner.java
@@ -5,7 +5,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-features="C:/Users/arahate/git/VacationExotica/src/main/java/features",
+//features="C:/Users/arahate/git/VacationExotica/src/main/java/features",
+features="C:/Users/japatel/git/VacationExotica/src/main/java/features",
 glue={"seleniumgluecode"},
 plugin = {"pretty", "html:target/cucumber-htmlreport","json:target/cucumber-report.json", "junit:target/cucumber_report.xml"},
 dryRun = false, //- make it true when you want to check Feature > StepDef mapping 
