8d57b572cf8e3bf2ce7abd932bd2507715f1a0b0
arahate <arahate@SWLL124.ap.corp.jwt.com>
2018-10-31 11:36:06 +0530

    Added about us scenario with code

--- src/main/java/runner/testrunner.java
+++ src/main/java/runner/testrunner.java
@@ -5,8 +5,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//features="C:/Users/arahate/git/VacationExotica/src/main/java/features",
-features="C:/Users/japatel/git/VacationExotica/src/main/java/features/Corporate.feature",
+features="C:/Users/arahate/git/VacationExotica/src/main/java/features/AboutUs.feature",
+//features="C:/Users/japatel/git/VacationExotica/src/main/java/features",
 glue={"seleniumgluecode"},
 plugin = {"pretty", "html:target/cucumber-htmlreport","json:target/cucumber-report.json", "junit:target/cucumber_report.xml"},
 dryRun = false, //- make it true when you want to check Feature > StepDef mapping 
